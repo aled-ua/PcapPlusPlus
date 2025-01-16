43b241f374684871b54460a9af2bd4002ee0bc3b
aled-ua <bugbuster.cc@gmail.com>
2025-01-16 13:34:04 +0800

    Fix format err

--- Packet++/src/TextBasedProtocol.cpp
+++ Packet++/src/TextBasedProtocol.cpp
@@ -297,11 +297,11 @@ namespace pcpp
 
 		std::string fieldName = fieldToRemove->getFieldName();
 		
-	        if ((fieldToRemove->m_NameOffsetInMessage + fieldToRemove->getFieldSize()) > m_DataLen)
-	        {
+		if ((fieldToRemove->m_NameOffsetInMessage + fieldToRemove->getFieldSize()) > m_DataLen)
+		{
 			PCPP_LOG_ERROR("Field range exceeds message length");
 			return false;
-	        }
+		}
 		
 		// shorten layer and delete this field
 		if (!shortenLayer(fieldToRemove->m_NameOffsetInMessage, fieldToRemove->getFieldSize()))
